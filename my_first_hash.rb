17a827481739a6f6830f347ee09dcde9453c8d86
Rachel T <rachmaybe@gmail.com>
2019-08-23 22:20:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
-def my_hash
+def my_hash(houses)
 houses = {"Harry" => Gryffindor, "Hermione" => Gryffindor, "Draco" = Slytherin}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
 end
 
 
@@ -10,8 +8,6 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
 end
 
 def retrieval
