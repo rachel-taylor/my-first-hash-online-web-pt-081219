e2f139444d40a5697ff75bbecc46be42261f9305
Rachel T <rachmaybe@gmail.com>
2019-08-23 22:20:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-houses = {}
+houses = {"Harry" => Gryffindor, "Hermione" => Gryffindor, "Draco" = Slytherin}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
