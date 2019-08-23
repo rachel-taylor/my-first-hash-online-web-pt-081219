582b5bb31f1f04c3d77298a218f394196cd59751
Rachel T <rachmaybe@gmail.com>
2019-08-23 22:43:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,17 +28,15 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest = {"muskets" => 2}
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+ shipping_manifest = {"gun powder" => 4}
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
